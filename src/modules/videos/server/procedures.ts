2d01202d55c847dbe5b5759fe550c021b700bf7e
renatomarf76 <renatomarf76@egmail.com>
2025-02-18 19:00:20 -0300

    video page

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,16 +1,36 @@
 import { z } from "zod";
-import { and, eq } from "drizzle-orm";
+import { and, eq, getTableColumns } from "drizzle-orm";
 import { UTApi } from "uploadthing/server";
 
 import { db } from "@/db";
 import { mux } from "@/lib/mux";
 import { workflow } from "@/lib/workflow"
-import { videos, videoUpdateSchema } from "@/db/schema";
+import { users, videos, videoUpdateSchema } from "@/db/schema";
 import { TRPCError } from "@trpc/server";
-import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { baseProcedure, createTRPCRouter, protectedProcedure } from "@/trpc/init";
 
 export const videosRouter = createTRPCRouter({
-  generateThumbnail: protectedProcedure
+  getOne: baseProcedure
+    .input(z.object({ id: z.string().uuid() }))
+    .query(async ({ input }) => {
+      const [existingVideo] = await db
+        .select({
+          ...getTableColumns(videos),
+          user: {
+            ...getTableColumns(users),
+          },
+        })
+        .from(videos)
+        .innerJoin(users, eq(videos.userId, users.id))
+        .where(eq(videos.id, input.id))
+
+      if (!existingVideo) {
+        throw new TRPCError({ code: "NOT_FOUND" });
+      }
+
+      return existingVideo;
+  }),
+  generateThumbnail: protectedProcedure // TODO: rever codigo integracao com deepseek ou openai
     .input(z.object({ id: z.string().uuid() }))
     .mutation(async ({ ctx, input }) => {
       const { id: userId } = ctx.user;
---
+++ src/modules/videos/ui/components/video-top-tow.tsx
@@ -0,0 +1,56 @@
+import { useMemo } from "react";
+import { format, formatDistanceToNow } from "date-fns";
+
+import { VideoMenu } from "./video-menu";
+import { VideoOwner } from "./video-owner";
+import { VideoReactions } from "./video-reactions";
+
+import { VideoGetOneOutput } from "../../types";
+import { VideoDescription } from "./video-description";
+
+interface VideoTopRowProps {
+  video: VideoGetOneOutput;
+}
+
+//TODO: implement whats left
+export const VideoTopRow = ({ video }: VideoTopRowProps) => {
+  const compactViews = useMemo(() => {
+    return Intl.NumberFormat("en", {
+      notation: "compact"
+    }).format(196437622);
+  }, []);
+
+  const expandedViews = useMemo(() => {
+    return Intl.NumberFormat("en", {
+      notation: "standard"
+    }).format(1600098);
+  }, []);
+
+  const compactDate = useMemo(() => {
+    return formatDistanceToNow(video.createdAt, { addSuffix: true });
+  }, [video.createdAt]);
+
+  const expandedDate = useMemo(() => {
+    return format(video.createdAt, "d MMM yyyy");
+  }, [video.createdAt]);
+
+  return ( 
+    <div className="flex flex-col gap-4 mt-4">
+      <h1 className="text-xl font-semibold">{video.title}</h1>
+      <div className="flex flex-col sm:flex-row sm:items-start sm:justify-between gap-4">
+        <VideoOwner user={video.user} videoId={video.id} />
+        <div className="flex overflow-x-auto sm:min-w-[calc(50%-6px)] sm:justify-end sm:overflow-visible pb-2 -mb-2 sm:pb-0 sm:mb-0 gap-2">
+          <VideoReactions />
+          <VideoMenu videoId={video.id} variant="secondary" />
+        </div>
+      </div>
+      <VideoDescription
+        compactViews={compactViews}
+        expandedViews={expandedViews}
+        compactDate={compactDate}
+        expandedDate={expandedDate}
+        description={video.description}
+      />
+    </div>
+   );
+}
